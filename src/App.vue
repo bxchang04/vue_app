210da769588392a39c46806cf6d91578c139244f
bxchang04 <bxchang04@gmail.com>
2019-11-30 19:51:40 -0500

    initialize

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
+/*
+Add a @dblclick attribute in the ToDo label element, which calls a method that sets the ToDo the user just double clicked as the edited ToDo
+Add an editing class to the list item that was double clicked: :class="{editing: todo == editedTodo}"
+Add some CSS styling to display:block or display:none based on the editing class you added in step 2.
+*/
+
+
+// src/App.vue
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <md-field>
+      <md-input v-model="currentTodo" @keydown.enter="addTodo()" placeholder="Add a todo"></md-input>
+    </md-field>
+
+    <ul class="todos">
+      <li v-for="todo in todos" :key="todo.id">
+        {{ todo.label }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      todos: [],
+      currentTodo: ''
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({id: this.todos.length, label: this.currentTodo, completed: false});
+      this.currentTodo = '';
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
+// src/main.js
 import Vue from 'vue'
 import App from './App.vue'
+import { MdField } from 'vue-material/dist/components';
+import 'vue-material/dist/vue-material.min.css';
+
+Vue.use(MdField);
+
 
 Vue.config.productionTip = false
 
