96f5c67168e1aa300303849309e83bfb0cad9c58
bxchang04 <bxchang04@gmail.com>
2019-12-21 23:22:48 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,32 @@
-/*
-Add a @dblclick attribute in the ToDo label element, which calls a method that sets the ToDo the user just double clicked as the edited ToDo
-Add an editing class to the list item that was double clicked: :class="{editing: todo == editedTodo}"
-Add some CSS styling to display:block or display:none based on the editing class you added in step 2.
-*/
-
-
-// src/App.vue
 <template>
-  <div>
-    <md-field>
-      <md-input v-model="currentTodo" @keydown.enter="addTodo()" placeholder="Add a todo"></md-input>
-    </md-field>
+  <div class="container">
+    <h1> Your Todo List </h1>
+    <md-card class="todoBox">
+      <md-field>
+        <md-input v-model="currentTodo" @keydown.enter="addTodo()" placeholder="Add an item to your todo list"></md-input>
+      </md-field>
 
-    <ul class="todos">
-      <li v-for="todo in todos" :key="todo.id">
-        {{ todo.label }}
-      </li>
-    </ul>
+      <div class="todos">
+        <li v-for="todo in todos" :key="todo.id">
+
+          <input class ='completeButton' type='checkbox' v-model="todo.completed">
+            <span
+            class="todo-item-label"
+            :class='{completed: todo.completed}'
+            @dblclick='editTodo(todo)'
+            v-if="!todo.edit">
+              {{todo.label}}
+            </span>
+            <md-input v-else class="todo-item-edit"
+            type="text" v-model='todo.label'
+            @keyup.enter="completedEdit(todo)">
+          </md-input>
+
+          <md-raised class='closeButton' @click="removeTodo(index)">X</md-raised>
+        </li>
+      </div>
+
+    </md-card>
   </div>
 </template>
 
@@ -25,17 +35,64 @@ export default {
   data() {
     return {
       todos: [],
-      currentTodo: ''
+      currentTodo: '',
+      editedTodo: null
     };
   },
   methods: {
     addTodo() {
-      this.todos.push({id: this.todos.length, label: this.currentTodo, completed: false});
-      this.currentTodo = '';
+      this.todos.push({
+        id: this.todos.length,
+        label: this.currentTodo,
+        completed: false,
+        edit: false
+        });
+      this.currentTodo = ''
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+    },
+    editTodo(todo) {
+     todo.edit = true;
+    },
+    completedEdit(todo) {
+      todo.edit = false;
     }
   }
 };
 </script>
 
 <style>
+li {
+  list-style: none;
+}
+.container {
+  margin: auto;
+  text-align: center;
+  width: 50%;
+}
+.completed {
+  text-decoration: line-through;
+}
+.todoBox {
+  font-size: 1.5em;
+  padding: 0px 5px 2px 5px;
+}
+.cardBox {
+padding: 5px;
+}
+.closeButton {
+  float: left;
+}
+.closeButton:hover {
+  cursor: pointer;
+  transition: 0.2s ease;
+    color: red
+}
+.completeButton {
+  float: right;
+}
+.todos {
+  margin: 10px;
+}
 </style>
